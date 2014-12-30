0c6ba7bbd18c09b0e32abfc9f2bca894192c77cd
H0ots <richard_blah@msn.com>
2014-12-30 17:17:45 -0600

    Consolidation and Classes
    Put everything into one .CSV and threw all of the search functions into
    a Search() class.  Also combine the inputs and coded the program to open
    the CSV one time.

--- itemSearch.py
+++ itemSearch.py
@@ -2,76 +2,69 @@
 import csv
 import requests
 
-# gets the item name from the user
-def getItem():
-    item = raw_input("Enter an item name: ")
-    return item
+# figured classes out bruh
+class Search:
 
-# gets the system name from the user
-def getSystem():
-    market = raw_input("Enter trade system: ")
-    return market
+    # gets the item name from the user
+    def userInput(self):
+        self.item = raw_input("Enter an item name: ")
+        self.market = raw_input("Enter trade system: ")
 
-# opens the typeids csv and finds the item id number
-def openItemCSV():
-    file = open('typeids.csv')
-    data = csv.reader(file)
-    itemID = getItem()
+    # opens the typeids csv and finds the item id number
+    def openCSV(self):
+        file = open('typeids.csv')
+        data = csv.reader(file)
+        itemID = self.item
+        sysID = self.market
 
-    for row in data:
-        if itemID == row[1]:
-            newitemID = row[2]
-            return newitemID
-    file.close()
+        for row in data:
+            if itemID == row[1]:
+                self.newitemID = row[0]
 
-# opens the solarids csv and finds the solar system id number
-def openSolarCSV():
-    file = open('solarids.csv')
-    data = csv.reader(file)
-    solarID = getSystem()
+            if sysID == row[3]:
+                self.newsysID = row[2]
+        file.close()
 
-    for row in data:
-        if solarID == row[1]:
-            newsolarID = row[0]
-            return newsolarID
-    file.close()
+    # generates the API url used to gather our json data
+    def getData(self):
+        base = "http://api.eve-central.com/api/marketstat/json"
+        payload = {'typeid': self.newitemID, 'usesystem': self.newsysID}
+        r = requests.get(base, params=payload)
+        r_data = r.json()
 
-# generates the API url used to gather our json data
-def getData():
-    base = "http://api.eve-central.com/api/marketstat/json"
-    payload = {'typeid': openItemCSV(), 'usesystem': openSolarCSV()}
-    r = requests.get(base, params=payload)
-    r_data = r.json()
+        min_sell = r_data[0]['sell']['min']     # gets minimum sell order data
+        max_buy = r_data[0]['buy']['max']       # gets maximum buy order data
+        gross_margin = min_sell - max_buy       # subtract buy and sell for profit (tax not included)
+        sales_tax = min_sell * 0.009            # tax is based on Product X skills in Dodixie market
+        brokers_fee = max_buy * 0.0059          # see above
+        taxes = sales_tax + brokers_fee         # combines sales tax and broker's fee for total tax
+        net_margin = gross_margin - taxes       # subtracts the tax from original profit
+        margin_perc = (net_margin / min_sell)   # calculates "worthiness"
 
-    min_sell = r_data[0]['sell']['min']     # gets minimum sell order data
-    max_buy = r_data[0]['buy']['max']       # gets maximum buy order data
-    gross_margin = min_sell - max_buy       # subtract buy and sell for profit (tax not included)
-    sales_tax = min_sell * 0.009            # tax is based on Product X skills in Dodixie market
-    brokers_fee = max_buy * 0.0059          # see above
-    taxes = sales_tax + brokers_fee         # combines sales tax and broker's fee for total tax
-    net_margin = gross_margin - taxes       # subtracts the tax from original profit
-    margin_perc = (net_margin / min_sell)   # calculates "worthiness"
+        # prints the relevant information
+        print r.url
+        print "Tax: ${:,.2f} \n".format(taxes)
+        print "Minimum Sell order: ${:,.2f}".format(min_sell)
+        print "Maximum Buy order: ${:,.2f}".format(max_buy)
+        print "Gross Margin: ${:,.2f}".format(gross_margin)
+        print "Total Profit (after taxes): ${:,.2f}".format(net_margin)
+        print "Margin Percentage: {:.2%} \n".format(margin_perc)
+        print "Summary:"
 
-    # prints the relevant information
-    print r.url
-    print "Tax: ${:,.2f} \n".format(taxes)
-    print "Minimum Sell order: ${:,.2f}".format(min_sell)
-    print "Maximum Buy order: ${:,.2f}".format(max_buy)
-    print "Gross Margin: ${:,.2f}".format(gross_margin)
-    print "Total Profit (after taxes): ${:,.2f}".format(net_margin)
-    print "Margin Percentage: {:.2%} \n".format(margin_perc)
-    print "Summary:"
+        if margin_perc >= .15:
+            print "BUY THIS SHIT!!!"
+        elif .07 <= margin_perc <= .149:
+            print "Not a bad investment"
+        # the market can swing either way, but this is generally an insignificant return
+        elif .03 <= margin_perc <= .069:
+            print "Little risk, little reward."
+        else:
+            print "Don't bother."
+        print "=" * 20
 
-    if margin_perc >= .15:
-        print "BUY THIS SHIT!!!"
-    elif .07 <= margin_perc <= .149:
-        print "Not a bad investment"
-    elif .03 <= margin_perc <= .069:  # the market can swing either way, but this is generally an insignificant return
-        print "Little risk, little reward."
-    else:
-        print "Don't bother."
-    print "\n=====================================================\n"
-
+s = Search()
 x = 0
 while x == 0:
-    getData()
+    s.userInput()
+    s.openCSV()
+    s.getData()
--- topmargin.py
+++ topmargin.py
@@ -1,6 +1,18 @@
 # This file's purpose is to calculate the top 25 item margins for a given trade system
 import csv
-import requests
+
+'''
+Build a function that gets the type ID for every fucking item, gathers the api data,
+then prints the top 25 margins.  I am limiting this function to the top 4 trade hubs
+listed above.  I suppose the data needs to be stored in an array, compared, and then printed.
+1. iterate through the csv for typeID's
+2. put these in URLS for data retrieval
+3. store this data into an array?
+4. sort items by margin percentage - greatest to least
+5. print top 25 items - "Item Name" , "Margin Percent"
+
+This is the ultimate goal and purpose of the program.
+'''
 
 def getSystem():
     market = raw_input("Enter a trade system: Amarr, Dodixie, Jita, or Rens\n")
@@ -17,18 +29,17 @@ def getSystem():
     print marketID
     return marketID
 
-'''
-Build a function that gets the type ID for every fucking item, gathers the api data, then prints the
-top 25 margins.  I am limiting this function to the top 4 trade hubs listed above.  I suppose the
-data needs to be stored in an array, compared, and then printed.
-1. iterate through the csv for typeID's
-2. put these in URLS for data retrieval
-3. store this data into an array?
-4. sort items by margin percentage - greatest to least
-5. print top 25 items - "Item Name" , "Margin Percent"
+def openItemCSV():
+    file = open('typeids.csv')
+    data = csv.reader(file)
+    itemID = []
 
-This is the ultimate goal and purpose of the program.
-'''
+    for row in data:
+        itemID = row[0]
+        print len(itemID)
+        print itemID
+    file.close()
 
 getSystem()
+openItemCSV()
 
